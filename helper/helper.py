192af6bd888326123ec06057df8c99d1222af093
Pablo Jorge Eduardo Rodriguez <pr@tekii.com>
2025-08-11 19:23:23 -0300

    wip helper beauty

--- helper/helper.py
+++ helper/helper.py
@@ -1,36 +1,44 @@
-import click,re
+"""helper"""
+import re
 from os import path
 from urllib.parse import urlparse
 from urllib.parse import urlunparse
+import click
+
 
 @click.command()
-@click.option('--base','base_',required=True,type=click.Path(exists=False),help='to be completed')
+@click.option('--base', 'base_', required=True, type=click.Path(exists=False), help='to be completed')
 @click.argument('input', type=click.File('r'))
 @click.argument('output', type=click.File('w'))
-def css_remap(base_,input,output):
+def css_remap(base_, input_, output):
+    """css-remap
+    rewrite css urls"""
     #
     def do_remap(mo):
         p = mo.group(0)
-        r = path.relpath(p,base_)
+        r = path.relpath(p, base_)
         return r
     #
-    input_css = input.read()
+    input_css = input_.read()
     pattern = re.compile(r'(?<=url\(\").+?(?=\"\))')
-    output_css = re.sub(pattern,do_remap,input_css)
+    output_css = re.sub(pattern, do_remap, input_css)
     output.write(output_css)
 
+
 @click.command()
-@click.option('--scheme','scheme_',required=True,type=click.Choice(['http', 'https'], case_sensitive=True))
-@click.option('--start','start_',required=True,type=click.Path(exists=False,allow_dash=False))
-@click.option('--path','path_',required=True,type=click.Path(exists=False,allow_dash=False))
-def url_absolute(scheme_,start_,path_):
-    url = path.relpath(path_, start_) 
+@click.option('--scheme', 'scheme_', required=True, type=click.Choice(['http', 'https'], case_sensitive=True))
+@click.option('--start', 'start_', required=True, type=click.Path(exists=False, allow_dash=False))
+@click.option('--path', 'path_', required=True, type=click.Path(exists=False, allow_dash=False))
+def url_absolute(scheme_, start_, path_):
+    """url-absolute"""
+    url = path.relpath(path_, start_)
     url = "//" + url
-    part = urlparse(url)  
-    result = urlunparse(part._replace(scheme = scheme_))
+    part = urlparse(url)
+    result = urlunparse(part._replace(scheme=scheme_))
     click.echo(click.format_filename(result))
+
 
 @click.command()
 def otro():
+    """otro"""
     click.echo('/* * this is a fake stylesheet * */')
-    pass
